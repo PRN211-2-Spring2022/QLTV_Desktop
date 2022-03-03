45298f5bfaba714776f17fa76cd053d6e23be8ac
DatLTHE151200 <97074281+DatLTHE151200@users.noreply.github.com>
2022-03-03 18:09:27 +0700

    Add models

---
+++ QLTV/Models/TbAcount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLTV.Models
+{
+    public partial class TbAcount
+    {
+        public string Gmail { get; set; }
+        public string Password { get; set; }
+        public int Manhanvien { get; set; }
+        public int Quyen { get; set; }
+
+        public virtual TbNhanVien ManhanvienNavigation { get; set; }
+    }
+}
