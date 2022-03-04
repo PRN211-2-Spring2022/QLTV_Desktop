53c6ada61db26f647101d51052cebd0132027b29
Dan Chen <72430280+DanielChendyrov@users.noreply.github.com>
2022-03-04 21:36:26 +0700

    demo commit

--- QLTV/Program.cs
+++ QLTV/Program.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace QLTV
 {
@@ -18,6 +18,8 @@ namespace QLTV
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Console.WriteLine("adb");
         }
     }
 }
