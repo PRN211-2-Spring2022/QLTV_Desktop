bd4120fbcc4cffdf7729e6c635b061baec50b2d4
Dan Chen <72430280+DanielChendyrov@users.noreply.github.com>
2022-03-12 16:01:26 +0700

    Reconfigure the QLTV_DesktopContext file to use appsettings.json instead of hardcoded connection string

---
+++ QLTV/BookManagement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using QLTV.Models;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace QLTV
+{
+    public partial class BookManagement : UserControl
+    {
+        public BookManagement()
+        {
+            InitializeComponent();
+        }
+
+        QLTV_DesktopContext QLTV_Desktop = new QLTV_DesktopContext();
+
+        public void LoadBooks()
+        {
+            var books = (
+                from b in QLTV_Desktop.TbDausaches
+                select new { b.Madausach, b.Tendausach, b.Soluong, b.Sotrang }
+            ).ToList();
+            txtSearch.DataBindings.Clear();
+
+            dgvBook.DataSource = books;
+        }
+
+        private void BookManagement_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadBooks();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error loading Data");
+            }
+        }
+    }
+}
