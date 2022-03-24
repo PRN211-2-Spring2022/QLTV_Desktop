71afe5e648b91bc2fafdc64c6553cfab3fc017e5
Ka Hánh <lqkhanh0@gmail.com>
2022-03-24 13:28:28 +0700

    ss

---
+++ QLTV/FrmMuonSach.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using QLTV.Models;
+
+namespace BookLoanManager
+{
+    public partial class FrmMuonSach : Form
+    {
+
+        public FrmMuonSach()
+        {
+            InitializeComponent();
+        }
+        
+        QLTV_DesktopContext qlms = new QLTV_DesktopContext();
+
+        public void Loadmuonsach()
+        {
+            lblMaSach.DataBindings.Clear();
+            lblMaLoai.DataBindings.Clear();
+            lblSoLuong.DataBindings.Clear();
+            lblMaTG.DataBindings.Clear();
+        }
+
+        private void FrmMuonSach_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Loadmuonsach();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void MuonTraSach_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dl = MessageBox.Show("Bạn có muốn đóng chương trình không?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dl == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void btnCreatePhieu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
