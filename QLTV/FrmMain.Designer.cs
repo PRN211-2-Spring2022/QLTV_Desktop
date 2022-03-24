917ece52cfd57c2bf04cfc5cffb3c1a070c84a05
Ka Hánh <lqkhanh0@gmail.com>
2022-03-24 21:20:39 +0700

    ss

--- QLTV/FrmMain.Designer.cs
+++ QLTV/FrmMain.Designer.cs
@@ -28,11 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.searchByCode = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnSearchByName = new System.Windows.Forms.Button();
-            this.txtSearchByName = new System.Windows.Forms.TextBox();
-            this.ListBorrower = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btntaophieu = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -49,72 +44,29 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // searchByCode
-            // 
-            this.searchByCode.Location = new System.Drawing.Point(211, 123);
-            this.searchByCode.Name = "searchByCode";
-            this.searchByCode.Size = new System.Drawing.Size(167, 29);
-            this.searchByCode.TabIndex = 14;
-            this.searchByCode.Text = "Tìm theo mã độc giả";
-            this.searchByCode.UseVisualStyleBackColor = true;
-            this.searchByCode.Click += new System.EventHandler(this.searchByCode_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(42, 123);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 27);
-            this.textBox1.TabIndex = 13;
-            // 
-            // btnSearchByName
-            // 
-            this.btnSearchByName.Location = new System.Drawing.Point(252, 166);
-            this.btnSearchByName.Name = "btnSearchByName";
-            this.btnSearchByName.Size = new System.Drawing.Size(126, 29);
-            this.btnSearchByName.TabIndex = 12;
-            this.btnSearchByName.Text = "Tìm theo tên";
-            this.btnSearchByName.UseVisualStyleBackColor = true;
-            this.btnSearchByName.Click += new System.EventHandler(this.btnSearchByName_Click);
-            // 
-            // txtSearchByName
-            // 
-            this.txtSearchByName.Location = new System.Drawing.Point(42, 168);
-            this.txtSearchByName.Name = "txtSearchByName";
-            this.txtSearchByName.Size = new System.Drawing.Size(187, 27);
-            this.txtSearchByName.TabIndex = 11;
-            // 
-            // ListBorrower
-            // 
-            this.ListBorrower.AutoSize = true;
-            this.ListBorrower.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.ListBorrower.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ListBorrower.Location = new System.Drawing.Point(35, 73);
-            this.ListBorrower.Name = "ListBorrower";
-            this.ListBorrower.Size = new System.Drawing.Size(207, 30);
-            this.ListBorrower.TabIndex = 10;
-            this.ListBorrower.Text = "Danh Sách Độc Giả";
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(35, 226);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 226);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(408, 345);
+            this.dataGridView1.Size = new System.Drawing.Size(375, 345);
             this.dataGridView1.TabIndex = 9;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btntaophieu
             // 
-            this.btntaophieu.Location = new System.Drawing.Point(921, 584);
+            this.btntaophieu.Location = new System.Drawing.Point(816, 519);
             this.btntaophieu.Name = "btntaophieu";
             this.btntaophieu.Size = new System.Drawing.Size(145, 29);
             this.btntaophieu.TabIndex = 18;
@@ -124,7 +76,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(921, 629);
+            this.button1.Location = new System.Drawing.Point(1027, 519);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 29);
             this.button1.TabIndex = 19;
@@ -145,9 +97,9 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(756, 187);
+            this.groupBox1.Location = new System.Drawing.Point(757, 85);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(422, 384);
+            this.groupBox1.Size = new System.Drawing.Size(425, 408);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin phiếu";
@@ -251,18 +203,27 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Ngày mượn";
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(404, 226);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(347, 345);
+            this.dataGridView2.TabIndex = 21;
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.searchByCode);
             this.Controls.Add(this.btntaophieu);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnSearchByName);
-            this.Controls.Add(this.txtSearchByName);
-            this.Controls.Add(this.ListBorrower);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FrmMain";
             this.Size = new System.Drawing.Size(1198, 661);
@@ -270,17 +231,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button searchByCode;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnSearchByName;
-        private System.Windows.Forms.TextBox txtSearchByName;
-        private System.Windows.Forms.Label ListBorrower;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btntaophieu;
         private System.Windows.Forms.Button button1;
@@ -297,5 +253,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
