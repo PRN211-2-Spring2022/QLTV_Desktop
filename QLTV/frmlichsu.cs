8eea1bccabd1993467e10b060e358458396099f6
Ka Hánh <lqkhanh0@gmail.com>
2022-03-25 20:56:30 +0700

    new

--- QLTV/frmlichsu.cs
+++ QLTV/frmlichsu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLTV.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,36 @@ namespace QLTV
         public frmlichsu()
         {
             InitializeComponent();
+        }
+
+        QLTV_DesktopContext qltv = new QLTV_DesktopContext();
+        public void loadlichsu()
+        {
+            var lichsu = (from s in qltv.TbPhieubangiaosaches
+                          select new
+                          {
+                              s.Maphieubangiao, s.Mathedocgia, s.Manhanvien, s.Ngaymuon, s.Ngaydukientra, s.Tinhtrangkhigiao
+                          }).ToList();
+            dgvlichsu.DataSource = lichsu;
+        }
+        private void frmlichsu_Load(object sender, EventArgs e)
+        {
+            loadlichsu();
+        }
+        public void loadsearch()
+        {
+            var phieu = qltv.TbPhieubangiaosaches.Where(m => m.Maphieubangiao == int.Parse(txtfind.Text)).ToList();
+            dgvlichsu.DataSource = phieu;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadsearch();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadlichsu();
         }
     }
 }
